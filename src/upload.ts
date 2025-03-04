f3feb5849d0613c137a0a7bccfc8692fc2cce187
Lucas Machado <mchadolucas@gmail.com>
2025-03-04 19:01:30 -0300

    feat: update S3 upload function to use image name as key and make s3Path optional

--- src/upload.ts
+++ src/upload.ts
@@ -4,7 +4,7 @@ const s3 = new AWS.S3();
 
 export const uploadImagesToS3 = async (
   images: { name: string; content: Buffer }[],
-  s3Path: string
+  _s3Path?: string
 ): Promise<string[]> => {
   if (!process.env.S3_BUCKET_NAME) {
     throw new Error("Variável de ambiente S3_BUCKET_NAME não definida.");
@@ -17,17 +17,16 @@ export const uploadImagesToS3 = async (
     let contentType = "image/jpeg";
     if (extension === "png") {
       contentType = "image/png";
-    } 
-    if(extension === "webp") {
-      contentType = "image/webp";
     }
-    else if (extension === "jpeg" || extension === "jpg") {
+    if (extension === "webp") {
+      contentType = "image/webp";
+    } else if (extension === "jpeg" || extension === "jpg") {
       contentType = "image/jpeg";
     }
 
     const uploadParams = {
       Bucket: process.env.S3_BUCKET_NAME!,
-      Key: `${s3Path}/${image.name}`,
+      Key: `${image.name}`,
       Body: image.content,
       ContentType: contentType,
     };
